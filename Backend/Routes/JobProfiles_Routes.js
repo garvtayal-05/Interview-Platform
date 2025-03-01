17d31b98c8eb9645975719e887632a09001aba97
Garv Tayal <garvtayal05.com>
2025-03-02 00:25:59 +0530

    Add GetJobProfile route in JobProfiles_Routes

--- Backend/Routes/JobProfiles_Routes.js
+++ Backend/Routes/JobProfiles_Routes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { CreateJobProfile, GetAllJobProfiles, generateQuestions } = require('../Controllers/JobProfile_Controller');
+const { CreateJobProfile, GetAllJobProfiles, generateQuestions, GetJobProfile } = require('../Controllers/JobProfile_Controller');
 const { checkforAuth, restrictTo } = require('../MiddleWares/MiddleAuth');
 const router = express.Router();
 
 router.post('/create', checkforAuth, restrictTo(['admin']), CreateJobProfile);
 router.get('/fetch', checkforAuth, GetAllJobProfiles);
+router.get('/fetch/:_id', checkforAuth, GetJobProfile);
 router.get('/fetch/:jobrole/generate-questions', checkforAuth, generateQuestions);
 
  
